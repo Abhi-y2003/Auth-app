06f70c463e9fa2d0b77f2968d268b6d7eeac74b8
Abhi-y2003 <abhisheks2651@gmail.com>
2024-04-09 17:11:16 +0530

    raw signup authh created

--- controller/Auth.js
+++ controller/Auth.js
@@ -4,3 +4,54 @@ const User = require("../models/user")
 
 //signup route handler
 
+exports.signup = async (req,res)=>{
+
+    try {
+        //get input data
+        const {name, email, password, role}  =  req.body; 
+        
+        //check if user already exist
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"user already exist"
+            });
+        }
+
+        
+        //securing password
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password, 10);
+
+        }
+         catch(error){
+            return res.status(500).json({
+                success:false,
+                message:"error in hashing password"
+            })
+        }
+
+
+        //creating entry for user
+        const user = await User.create({
+            name, email, password:hashedPassword, role
+        })
+
+
+        return res.status(200).json({
+            success:true,
+            message:"user created successfully"
+        })
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+                success:false,
+                message:"user is not register"
+            })
+       
+    }
+}
